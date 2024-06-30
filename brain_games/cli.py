0d10272c340994dfb57ece26399599d7df1e5e7f
Ilfat Shaydullin <sweden.fan@yandex.ru>
2024-06-30 22:05:10 +0300

    modifed cli.py

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -5,6 +5,7 @@ def welcome_user():
     print("Welcome to the Brain Games!")
     name = prompt.string("May I have your name? ")
     print(f"Hello, {name}")
+    return(name)
 
 
 # Две пустые строки
