c9aec7d1aeb862f69d3a6066dbca6f1f7d6c2fc1
Ilfat Shaydullin <sweden.fan@yandex.ru>
2024-07-05 14:52:24 +0300

    modifed utils.py

--- brain_games/utils.py
+++ brain_games/utils.py
@@ -12,3 +12,11 @@ def get_user_answer():
 
 def is_game_complete(target_score, target_score_needed):
     return target_score < target_score_needed
+
+def gcd(num_one, num_two):
+    while num_one != num_two:
+        if num_one > num_two:
+            num_one = num_one - num_two
+        else:
+            num_two = num_two - num_one
+    return(num_one)
