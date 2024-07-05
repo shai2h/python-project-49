ab973657377fbfeba85535c83bb76f4f3f0e2950
Ilfat Shaydullin <sweden.fan@yandex.ru>
2024-07-05 11:37:51 +0300

    modifed utils.py

--- brain_games/utils.py
+++ brain_games/utils.py
@@ -4,3 +4,11 @@ from brain_games.cli import welcome_user
 
 
 
+def get_random_number(start=1, end=20):
+    return random.randint(start, end)
+
+def get_user_answer():
+    return input('Your answer: ').lower().strip()
+
+def is_game_complete(target_score, target_score_needed):
+    return target_score < target_score_needed
