d269662a27f78d49984bf7604b62c32f22e23bf7
Ilfat Shaydullin <sweden.fan@yandex.ru>
2024-07-10 15:43:02 +0300

    modifed code in brain_even.py file

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -8,33 +8,35 @@ from brain_games.utils import (
 
 def brain_even(name):
 
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-
     target_score = 0
     target_score_needed = 3
     yes_answer = 'yes'
     no_answer = 'no'
 
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+
     while is_game_complete(target_score, target_score_needed):
         random_num = get_random_number(0, 20)
         print(f'Question: {random_num}')
         user_answer = get_user_answer()
+
         if (user_answer != yes_answer) and (user_answer != no_answer):
             print(f"Let's try again, {name}!")
             break
 
-        if (
-            (random_num % 2 == 0 and user_answer.lower() == yes_answer)
-            or (random_num % 2 != 0 and user_answer.lower() == no_answer)
-        ):
+        if random_num % 2 == 0:
+            correct_answer = yes_answer
+        else:
+            correct_answer = no_answer
+
+        if user_answer == correct_answer:
             print('Correct!')
             target_score += 1
-
-        elif (
-            (random_num % 2 != 0 and user_answer.lower() == yes_answer)
-            or (random_num % 2 == 0 and user_answer.lower() == no_answer)
-        ):
-            print("'no' is wrong answer ;(. Correct answer was 'yes'.")
+        else:
+            print(
+                f"'{user_answer}' is wrong answer ;(."
+                f"Correct answer was '{correct_answer}'."
+            )
             print(f"Let's try again, {name}!")
             break
 
