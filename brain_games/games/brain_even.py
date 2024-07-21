47e1b6d5ea6153c92967f21cd14c51127230cfb5
Shaydullin Ilfat <sweden.fan@Yandex.ru>
2024-07-21 22:13:19 +0300

    added new function

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -6,24 +6,28 @@ from brain_games.utils import (
 )
 
 
+def question_even():
+    yes_answer = 'yes'
+    no_answer = 'no'
+    random_num = get_random_number(0, 20)
+    if random_num % 2 == 0:
+        correct_answer = yes_answer
+    else:
+        correct_answer = no_answer
+    return correct_answer, random_num
+
+
 def brain_even(name):
 
     target_score = 0
     target_score_needed = 3
-    yes_answer = 'yes'
-    no_answer = 'no'
 
     print('Answer "yes" if the number is even, otherwise answer "no".')
 
     while is_game_complete(target_score, target_score_needed):
-        random_num = get_random_number(0, 20)
+        correct_answer, random_num = question_even()
         print(f'Question: {random_num}')
         user_answer = get_user_answer()
-
-        if random_num % 2 == 0:
-            correct_answer = yes_answer
-        else:
-            correct_answer = no_answer
 
         if user_answer == correct_answer:
             print('Correct!')
