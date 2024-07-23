263927e2afb08046055d8641fbf8b1a6b50e8415
Ilfat Shaydullin <sweden.fan@yandex.ru>
2024-07-23 15:22:00 +0300

    fixed brain-progression

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -5,14 +5,12 @@ from brain_games.cli import welcome_user
 
 def is_prime():
     random_num = get_random_number(1, 20)
-    correct_answer = ''
     if random_num < 2:
-        correct_answer = 'no'
+        return random_num, 'no'
     for i in range(2, int(random_num ** 0.5) + 1):
         if random_num % i == 0:
-            correct_answer = 'no'
-    correct_answer = 'yes'
-    return random_num, correct_answer
+            return random_num, 'no'
+    return random_num, 'yes'
 
 
 def brain_prime():
